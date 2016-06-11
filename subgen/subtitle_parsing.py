b68f3b3b8545320e6c24270a3ca7b6f13e532397
Ying-Shu Kuo <hank5925@gmail.com>
2016-06-11 22:31:58 +0800

    subtitle_parsing.py, parse .ass file into lists of timestamps.

---
+++ subgen/subtitle_parsing.py
@@ -0,0 +1,35 @@
+import math
+import datetime
+from subgen.subtitle import Subtitle
+
+def time_parse(s):
+    hour, minute, second_decimal = [t for t in s.split(':')]
+    second, microsec = second_decimal.split('.')
+    microsec = microsec.ljust(6, '0')
+    return [int(hour), int(minute), int(second), int(microsec)]
+
+def subtitle_parser(sub_file):
+    res_start = []
+    res_end = []
+    with open(sub_file, 'r') as f:
+        inEvents = False
+        ss = f.readlines()
+        for s in ss:
+            if inEvents == True:
+                sl = s.strip().split(',')
+                if sl[1] != ' Start':
+                    rs = time_parse(sl[1])
+                    re = time_parse(sl[2])
+                    res_start.append(rs)
+                    res_end.append(re)
+            if s.strip() == "[Events]":
+                inEvents = True
+    return res_start, res_end
+
+#if __name__ == "__main__":
+    #import sys
+    #import os
+    #if os.path.exists(sys.argv[1]) == True:
+        #subtitle_parser(sys.argv[1])
+    #else:
+        #raise FileNotFoundError(sys.argv[0] + ": " + sys.argv[1] + "not found.")
